06db9e2037146abb166c63c6c628d4fe93ad0fae
Donat Baier <donat.baier@gmail.com>
2013-01-12 14:26:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Regalodeboda::Application.config.secret_token = 'b8eb032007d1af0e47858b1563e59307627c4d470fece829b5892508b2fa1e8f7e779f52a5db2fe8557fde0c30498397a1670449bba9fd66a7ce81c22c94b76b'
